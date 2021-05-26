945c2f2c8a838d8a47e7ec57d9aac8d841b8c39b
Jon Rickard Yxelflod <rickard@yxelflod.se>
2021-05-26 09:32:25 +0200

    css tweaks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
+  const [jokes, setJokes] = useState([]);
+  const [selectJoke, setSelectJoke] = useState({});
+
+  useEffect(() => {
+    async function getJokes() {
+      const response = await fetch('https://official-joke-api.appspot.com/random_ten');
+      const data = await response.json();
+
+      setJokes(data);
+    }
+
+    getJokes();
+  }, []);
+
+  function getRandomJoke() {
+    const joke = jokes[Math.floor(Math.random() * jokes.length)];
+    setSelectJoke(joke);
+  }
+
+  useEffect(() => {
+    getRandomJoke();
+  }, [jokes]);
+
+	return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { selectJoke ?
+      <article id="joke">
+        <p id="setup">{ selectJoke.setup }</p>
+        <p id="punchline">{ selectJoke.punchline }</p>
+        <button id="shuffle" onClick={ getRandomJoke }>Shuffle</button>
+      </article>
+      : '' }
     </div>
   );
 }
